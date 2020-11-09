cce129a1dab72c0f1b5435cd6148378884672cee
aleksandGG <aleksandrgaranin89@gmail.com>
2020-11-09 13:56:55 -0800

    added cancelToken to axios request

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -25,14 +25,21 @@ const reducer = (state, action) => {
 export default function useFetchJobs(params, page) {
     const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true })
     useEffect(() => {
+        const cancelToken = axios.CancelToken.source()
         dispatch({ type: ACTIONS.MAKE_REQUEST })
         axios.get(BASE_URL, {
+            cancelToken: cancelToken.token,
             params: { markdown: true, page: page, ...params }
         }).then(res => {
             dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } })
         }).catch(err => {
+            if(axios.isCancel(err)) return
             dispatch({type: ACTIONS.ERROR, payload: {error:err}})
         })
+
+        return () => {
+            cancelToken.cancel()
+        }
     }, [params, page])
     return state
 }
