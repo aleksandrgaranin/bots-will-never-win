acbf503a5caf9ce4fcaa5ebc338ad89fbb981fcc
aleksandGG <aleksandrgaranin89@gmail.com>
2020-11-09 13:47:41 -0800

    Created custom Hook + fetching logic by using useReducer, useEffect + axios + around COSR error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import useFetchJobs from './useFetchJobs';
+import { Container } from 'react-bootstrap'
+
 
 function App() {
+  const { jobs, loading, error } = useFetchJobs()
   return (
-    <div>
-      <h1>Hi there</h1>
-    </div>
+    <Container>
+      {loading && <h1>...loading...</h1>}
+      {error && <h1>Error...something went wrong</h1>}
+      <h1>{jobs.length}</h1>
+    </Container>
   );
 }
 
