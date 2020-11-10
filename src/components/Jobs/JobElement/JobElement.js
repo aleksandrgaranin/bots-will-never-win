d7a40ddf2293593ede1529281fa6f8cd8a4c8ff4
aleksandGG <aleksandrgaranin89@gmail.com>
2020-11-09 19:01:25 -0800

    Detail Modal added

--- src/components/Jobs/JobElement/JobElement.js
+++ src/components/Jobs/JobElement/JobElement.js
@@ -1,31 +1,64 @@
 
 import React, { useState } from 'react';
-import { Badge, Button, Collapse, Card } from 'react-bootstrap';
+import { Badge, Button, Collapse, Card, Modal } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown'
 
-const JobElement = ({ job }) => {
-    const [showMore, setShowMore] = useState(false);
+// const JobElement = ({ job }) => {
+//     const [showMore, setShowMore] = useState(false);
 
+//     return (
+//         <Card className="mb-2">
+//             <Card.Header as="h5">{job.title}</Card.Header>
+//             <Card.Body>
+//                 <Card.Title>Company: {job.company}</Card.Title>
+//                 <Card.Text>
+//                     <Badge variant="secondary">{job.location}</Badge>
+//                     <Badge variant="success">{job.type}</Badge>
+//                 </Card.Text>
+//                 <Button calssName="mb-4" onClick={() => setShowMore(prevShow => !prevShow)}>
+//                     {showMore ? 'Hide Details' : 'Show Details' }
+//                 </Button>
+//                 <Collapse in={showMore}>
+//                     <div>
+//                         <ReactMarkdown source={job.description} />
+//                     </div>
+//                 </Collapse>
+//             </Card.Body>
+//         </Card>
+//     )
+// }
+
+// export default JobElement
+
+const MyVerticallyCenteredModal = (props) => {
+    const [showMore, setShowMore] = useState(false);
     return (
-        <Card className="mb-2">
-            <Card.Header as="h5">{job.title}</Card.Header>
-            <Card.Body>
-                <Card.Title>Company: {job.company}</Card.Title>
-                <Card.Text>
-                    <Badge variant="secondary">{job.location}</Badge>
-                    <Badge variant="success">{job.type}</Badge>
-                </Card.Text>
-                <Button calssName="mb-4" onClick={() => setShowMore(prevShow => !prevShow)}>
-                    {showMore ? 'Hide Details' : 'Show Details' }
-                </Button>
-                <Collapse in={showMore}>
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    {props.job.title}
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <h5>Company: {props.job.company}</h5>
+                <Badge variant="secondary">{props.job.location}</Badge>
+                <Badge variant="success">{props.job.type}</Badge>
+                <br/>                
                     <div>
-                        <ReactMarkdown source={job.description} />
+                        <ReactMarkdown source={props.job.description} />
                     </div>
-                </Collapse>
-            </Card.Body>
-        </Card>
-    )
+                
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
 }
 
-export default JobElement
+export default MyVerticallyCenteredModal
--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -1,20 +1,55 @@
 import React, { useState } from 'react';
 import Table from 'react-bootstrap/Table';
 
-import JobElement from './JobElement/JobElement'
+import MyVerticallyCenteredModal from './JobElement/JobElement'
 
 export default function Jobs({ jobs }) {
-    
+    const [modalShow, setModalShow] = useState(false);
+    const [modalJob, setModalJob] = useState({})
+
+    const showModalHandler = (job) => {        
+        setModalJob(job)
+        setModalShow(true)
+    }
+    const closeModal = () => {
+        setModalShow(false)
+        setModalJob({})
+    }
+
     return (
-        <Table striped hover>
-            <thead>
-                {jobs.map(job => {
-                    console.log(job)
-                    return (<tr key={job.id}>
-                        <JobElement job={job} style={{}}/>
-                    </tr>)
-                })}
-            </thead>
-        </Table>
+        <div>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Company Name</th>
+                        <th>Date</th>
+                        <th>Location</th>
+                        <th>Type</th>
+                    </tr>
+                    {jobs.map(job => {
+                        // console.log(job)
+                        return (
+
+                            <tr key={job.id} onClick={() => showModalHandler(job)} >
+                                {/* <JobElement job={job} /> */}
+                                <td>{job.title}</td>
+                                <td>{job.company}</td>
+                                <td>{new Date(job.created_at).toLocaleDateString()}</td>
+                                <td>{job.location}</td>
+                                <td>{job.type}</td>
+                            </tr>
+                        )
+
+                    })}
+
+                </thead>
+            </Table>
+            <MyVerticallyCenteredModal
+                job={modalJob}
+                show={modalShow}
+                onHide={closeModal}
+            />
+        </div>
     )
 }
